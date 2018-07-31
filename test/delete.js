d071baa2f5674be8accb346612ebc9d44308497d
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-07-30 20:47:18 -1000

    passing test for multi-batch deletes

--- test/delete.js
+++ test/delete.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var memdb = require('memdb')
 var umbr = require('../')
 
-test('delete', function (t) {
+test('single batch delete', function (t) {
   t.plan(3)
   var br = umbr(memdb())
   var rows = [
@@ -19,4 +19,35 @@ test('delete', function (t) {
       t.deepEqual(ids.sort(), ['b','c'])
     })
   })
+})
+
+test('multi-batch delete', function (t) {
+  t.plan(5)
+  var br = umbr(memdb())
+  var batches = [
+    [
+      { type: 'put', id: 'a', refs: [] },
+      { type: 'put', id: 'b', refs: ['a'] },
+      { type: 'put', id: 'c', refs: ['a'] },
+      { type: 'put', id: 'd', refs: ['a'] },
+    ],
+    [
+      { type: 'del', id: 'd', links: ['d'] }
+    ]
+  ]
+  ;(function next (i) {
+    if (i === batches.length) return check()
+    br.batch(batches[i], function (err) {
+      t.error(err)
+      next(i+1)
+    })
+  })(0)
+
+  function check (err) {
+    t.error(err)
+    br.get('a', function (err, ids) {
+      t.error(err)
+      t.deepEqual(ids.sort(), ['b','c'])
+    })
+  }
 })
